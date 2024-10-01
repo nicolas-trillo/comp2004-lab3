9eb09a1da77b97d176f9be5a6d23cea4592e842a
Nicolastrillo <nicolastrillo@icloud.com>
2024-10-01 13:15:44 -0400

    submission

---
+++ src/components/ColorBox.jsx
@@ -0,0 +1,5 @@
+export default function ColorBox(color) {
+    // javascript stuff
+    // console.log(color.color)
+    return <div className="color-div" style={{backgroundColor: color.color}}></div>
+}
