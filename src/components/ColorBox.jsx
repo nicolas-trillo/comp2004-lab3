0dd7fa3b027f0ce6277c4d642c2a0ca0738b247a
nicolas-trillo <nicolastrillo@icloud.com>
2024-10-01 22:14:14 -0400

    second submission

--- src/components/ColorBox.jsx
+++ src/components/ColorBox.jsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import data from "../data/data";
+
 export default function ColorBox(color) {
     // javascript stuff
     // console.log(color.color)
-    return <div className="color-div" style={{backgroundColor: color.color}}></div>
+    const [bgColor, setBgColor] = useState(color.color);
+    function changeColor() {
+        setBgColor(data[Math.floor(Math.random() * data.length)]);
+    }
+    return (
+        <div
+            className="color-div"
+            style={{ backgroundColor: bgColor }}
+            onClick={changeColor}
+        ></div>
+    );
 }
--- src/components/ColorBoxesContainer.jsx
+++ src/components/ColorBoxesContainer.jsx
@@ -1,22 +1,26 @@
 import { useState } from "react";
-import data from '../data/data';
-import { v4 as uuid } from 'uuid';
+import data from "../data/data";
+import { v4 as uuid } from "uuid";
 import ColorBox from "./ColorBox";
 
 export default function ColorBoxesContainer() {
-    const initial = new Array(25).fill().map(() => data[Math.floor(Math.random() * data.length)]);
-    const [colors, setColors] = useState(initial);
-    function shuffleColors() {
-        setColors(new Array(25).fill().map(() => data[Math.floor(Math.random() * data.length)]))
-    }
-    return <div className="color-container" onClick={shuffleColors}>
-    {colors.map((color) => (
-        <ColorBox
-            // uses uuid so javascript won't cry about unique keys.
-            // overkill? yes.
-            key={uuid()} 
-            color={color}
-        />
-    ))}
-    </div>
+    const initial = new Array(25)
+        .fill()
+        .map(() => data[Math.floor(Math.random() * data.length)]);
+    const [colors /*, setColors*/] = useState(initial);
+    // function shuffleColors() {
+    //     setColors(new Array(25).fill().map(() => data[Math.floor(Math.random() * data.length)]))
+    // }
+    return (
+        <div className="color-container">
+            {colors.map((color) => (
+                <ColorBox
+                    // uses uuid so javascript won't cry about unique keys.
+                    // overkill? yes.
+                    key={uuid()}
+                    color={color}
+                />
+            ))}
+        </div>
+    );
 }
